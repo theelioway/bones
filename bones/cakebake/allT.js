2159e69f467a15aada38b33c7c325b2263dd38b6
Tim Bushell <tcbushell@gmail.com>
2020-12-01 13:12:10 +0200

    cakebake installed

--- bones/cakebake/allT.js
+++ bones/cakebake/allT.js
@@ -19,32 +19,7 @@ apiRouter.use(`/Thing`, crudRouter)
 * @returns {bonesApiResponse} the REST API format, the elioWay.
 */
 "use strict"
-
-//inital
 const Cakebase = require('cakebase')("../database.json");
-
-//Create a new row
-Cakebase.set({ id: 0, email: "..." });
-
-//Get rows
-const rows = Cakebase.get(e => e.email === "...");
-
-//Remove rows
-Cakebase.remove(rows);
-
-//Update rows
-Cakebase.update(e => e.id === "e1fe3...", { email: "..." });
-
-//Get all rows
-
-
-//Clear the whole database
-Cakebase.clear();
-
-const Cakebase = require('cakebase')("../database.json");
-var things = new Datastore();
-const { getError } = require("../utils/responseMessages")
-const settings = require("../settings")
 
 module.exports = Thing => {
   return async (req, res) => {
