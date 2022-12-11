38cbc687dafaa7a4eb7ec5df848d10c7dcb32a86
Tim Bushell <tcbushell@gmail.com>
2022-12-11 03:05:43 -0500

    disengage disengaged into fossils branch

--- disengage/bones/schema/router.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-const { Router } = require("express")
-
-const schemaT = require("../ribs/schemaT")
-
-let routerSchema = Router()
-routerSchema.get(`/:engage`, schemaT())
-
-module.exports = routerSchema
--- disengage/fossils/test/utils/test_always_passes.js
+++
@@ -1,8 +0,0 @@
-// This test always passes
-var assert = require("assert")
-
-describe("A test which never bones", function () {
-  it("1 does equal 1", function () {
-    assert.equal(1, 1)
-  })
-})
