6818288ae73a4e0bb348cffa7f7b4acdc6f475db
Tim Bushell <tcbushell@gmail.com>
2023-01-04 14:51:02 -0500

    smicks

--- flesh/index.js
+++ flesh/index.js
@@ -1,6 +1,6 @@
 /** @tutorial The simplest implementation of an elioFlesh layer. This could be
- * as sophisticated as a React app, or as simple as printing the
- * `payload` to the command line. */
+ * as sophisticated as a React app, or as simple as printing the `payload` to
+ * the command line. */
 
 module.exports = function flesh(statusCode, payload) {
   console.assert("statusCode:", statusCode)
